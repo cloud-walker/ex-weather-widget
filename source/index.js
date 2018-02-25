1dd43187e8eed410e01dd675281f491bee076500
Luca Barone <baro.luc@gmail.com>
2018-02-25 13:49:05 +0100

    Move Root component into the features folder

--- source/index.js
+++ source/index.js
@@ -1,12 +1,12 @@
 import {render} from 'react-dom'
 import React from 'react'
 
-import {Root} from '~/components/Root'
+import {Root} from '~/features/Root'
 
 const doRender = () => render(<Root />, document.getElementById('root'))
 
 doRender()
 
 if (module.hot) {
-  module.hot.accept('~/components/Root', () => doRender())
+  module.hot.accept('~/features/Root', () => doRender())
 }
