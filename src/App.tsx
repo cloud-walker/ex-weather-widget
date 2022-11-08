4f9fee787744900eabcd91d56322c1b9a398341a
Luca Barone <baro.luc@gmail.com>
2022-11-08 23:03:28 +0100

    migrate to tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import {HTMLAttributes, useEffect, useState} from 'react'
 import {MdFlipToBack, MdFlipToFront} from 'react-icons/md'
 import {WiCloudy, WiHumidity, WiWindy} from 'react-icons/wi'
 
-import classes from './App.module.css'
 import {envVars} from './envVars'
 import {getWeather} from './models/weather/weather'
 
@@ -23,32 +22,31 @@ export function App() {
     setSide(side == 'front' ? 'back' : 'front')
   }
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
+    navigator.geolocation.getCurrentPosition((pos) => {
       setPosition(pos)
     })
   }, [])
   return (
     <div className="h-full flex items-center justify-center">
       <div
-        className="grid auto-rows-fr w-[400px] transition-all data-[side=back]:rotate-180"
-        style={{transformStyle: 'preserve-3d', perspective: '1000px'}}
+        className={clsx(
+          'grid auto-rows-fr',
+          'w-[400px]',
+          'transition-all',
+          'data-[side=back]:[transform:rotateY(180deg)]',
+          '[transform-style:preserve-3d] [perspective:1000px]',
+        )}
         data-side={side}
       >
         <Info
           position={position}
           onFlip={handleFlip}
-          className="rounded"
-          style={{gridRow: '1 / -1', backfaceVisibility: 'hidden', zIndex: 1}}
+          className="row-[1/-1] col-[1/-1] rounded [backface-visibility:hidden] z-10"
         />
         <Map
           position={position}
           onFlip={handleFlip}
-          className="rounded"
-          style={{
-            gridRow: '1 / -1',
-            backfaceVisibility: 'hidden',
-            transform: 'rotateY(180deg)',
-          }}
+          className="row-[1/-1] col-[1/-1] rounded [backface-visibility:hidden] [transform:rotateY(180deg)]"
         />
       </div>
     </div>
@@ -79,16 +77,19 @@ function Info({
     enabled: params != null,
   })
   return (
-    <section {...props} className={clsx(className, classes.info)}>
-      <button onClick={onFlip}>
+    <section
+      {...props}
+      className={clsx(className, 'flex flex-col gap-2', 'p-4', 'bg-white')}
+    >
+      <button onClick={onFlip} className="self-end">
         <MdFlipToBack />
       </button>
-      <div className={clsx(classes['meteo-icon'], 'flex-grow')}>
+      <div className={clsx('flex-grow', 'flex items-center justify-center')}>
         <div>{query.isSuccess ? query.data?.weather[0].id : 'Loading...'}</div>
       </div>
       <div>
         {query.isSuccess ? (
-          <div className={classes.meteo}>
+          <div className="flex items-center gap-2">
             <div>{query.data?.main.temp}</div>
             <div className="flex-grow">
               <div>{query.data?.weather[0].description}</div>
@@ -102,16 +103,16 @@ function Info({
       </div>
       <div>
         {query.isSuccess ? (
-          <div className={classes.details}>
-            <div>
+          <div className="flex gap-2 justify-evenly">
+            <div className="flex gap-1">
               <WiWindy />
               {query.data?.wind.speed}
             </div>
-            <div>
+            <div className="flex gap-1">
               <WiHumidity />
               {query.data?.main.humidity}
             </div>
-            <div>
+            <div className="flex gap-1">
               <WiCloudy />
               {query.data?.clouds.all}
             </div>
@@ -148,7 +149,16 @@ function Map({
   url.searchParams.sort()
 
   return (
-    <div {...props} className={clsx(className, classes.map)}>
+    <div
+      {...props}
+      className={clsx(
+        className,
+        'relative',
+        'flex items-start justify-end',
+        'p-4',
+        'min-h-[400px]',
+      )}
+    >
       <iframe
         width="100%"
         height="100%"
@@ -157,8 +167,9 @@ function Map({
         allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
         src={url.toString()}
+        className="absolute inset-0"
       />
-      <button onClick={onFlip}>
+      <button onClick={onFlip} className="relative">
         <MdFlipToFront />
       </button>
     </div>
