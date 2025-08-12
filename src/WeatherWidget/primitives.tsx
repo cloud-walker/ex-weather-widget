c7cfcfa36fdfc63ca366262643e809607b1f4f06
Luca Barone <baro.luc@gmail.com>
2025-08-12 14:22:25 +0200

    add linting & formatting

--- src/WeatherWidget/primitives.tsx
+++ src/WeatherWidget/primitives.tsx
@@ -1,17 +1,17 @@
 import clsx from 'clsx'
-import {HTMLAttributes} from 'react'
+import type {HTMLAttributes} from 'react'
 
 export function ButtonFlip({
-  className,
-  ...props
+	className,
+	...props
 }: HTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      className={clsx(
-        className,
-        'text-3xl p-1 bg-gray-700 hover:bg-gray-800 text-white rounded-md',
-      )}
-      {...props}
-    />
-  )
+	return (
+		<button
+			className={clsx(
+				className,
+				'text-3xl p-1 bg-gray-700 hover:bg-gray-800 text-white rounded-md',
+			)}
+			{...props}
+		/>
+	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {createRoot} from 'react-dom/client'
 import {App} from './App'
 
 createRoot(getBySelector('#root')).render(
-  <QueryClientProvider client={new QueryClient()}>
-    <App />
-  </QueryClientProvider>,
+	<QueryClientProvider client={new QueryClient()}>
+		<App />
+	</QueryClientProvider>,
 )
